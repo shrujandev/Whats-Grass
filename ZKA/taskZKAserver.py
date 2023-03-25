eeb098cb5e9ce05a4f82387c082f54f0825d9d67
Shishira Bhat O <shishirabhato2@gmail.com>
2023-03-26 04:50:43 +0530

    Implemented Logging

--- ZKA/taskZKAserver.py
+++ ZKA/taskZKAserver.py
@@ -11,16 +11,24 @@ BUFFER_SIZE = 4096
 host = ''
 port = 9994
 
+import logging
+logging.basicConfig(filename=".\logfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 class ZKAThread(threading.Thread):
     def __init__(self, clientAddr, clientSocket):
         threading.Thread.__init__(self)
         self.cSocket = clientSocket
         self.cAddr = clientAddr
+        logger.info("Connection added on taskZKA server")
         print("New connection added :", clientAddr)
 
     def run(self):
         print("\nConnction from ", self.cAddr)
+        logger.info("Connection established with ZKA server")
         data = self.cSocket.recv(1024).decode()
 
         if data == "request":
@@ -36,15 +44,18 @@ class ZKAThread(threading.Thread):
                         # f.write(row+'\n')
             
             f = open("selected_list.csv", "w")
+            logger.info("Opened Selected List File to write into")
             data=('\n'.join(','.join(x) for x in selected_lines))
             f.write(data)
             f.close()
             sendFile("selected_list.csv", self.cSocket)
+            logger.info("Sent Ad list to server")
             print("Sent AD list to server")
             # self.cSocket.send(';'.join(selected_lines))
         else:
             self.cSocket.send(data.encode())
         self.cSocket.close()
+        logger.info("Connection closed with ZKA server")
         print("\nConnction from ", self.cAddr, " closed Successfully")
 
 def vpnZKA():
